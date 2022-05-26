dc92f5d265df81af65d6425292bf476b3d14397a
playchuqing <playchuqing1@163.com>
2022-05-27 00:14:52 +0800

    first commit

---
+++ View/index.js
@@ -0,0 +1,20 @@
+
+var listinfo = [
+    {
+        use_ImgUrl: "头像图片",
+        user_Name: "用户名称",
+        dateTime: "发布时间",
+        listTitle: "文章标题",
+        // 正文、段落
+        // Mysql 数组 push方法
+        InfoArr:[
+            
+        ]
+    }
+];
+function click() {
+    var btn = document.getElementById('dome');
+    btn.onclick = function () {
+        alert("aaa");
+    };
+}
---
+++ View/js.md
@@ -0,0 +1,11 @@
+# js的一些提示
+## js实现跳转
+跳转方法一：
+在本窗体打开页面
+link="'+link[i]+'";可以传递的参数
+``` location.href='./Html/article.html?link="'+link[i]+'"'; ``` 
+跳转方法一：
+在新窗体打开页面
+link="'+link[i]+'";可以传递的参数
+``` window.open('./Html/article.html?link="'+link[i]+'"'); ``` 
+
